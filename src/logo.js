d5c694c50ac6bcba568a06ea8f8696e0a7827546
goHere <clemay@gohere.tech>
2023-01-26 12:00:52 -0600

    runnit

--- src/logo.js
+++ src/logo.js
@@ -1,7 +1,7 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.148.0/build/three.module.js";
+import * as THREE from "three";
 import * as dat from "https://cdn.jsdelivr.net/npm/dat.gui@0.7.9/build/dat.gui.module.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.148.0/examples/jsm/loaders/GLTFLoader.js";
-import { RGBELoader } from "https://cdn.jsdelivr.net/npm/three@0.148.0/examples/jsm/loaders/RGBELoader.js";
+import { GLTFLoader } from "GLTFLoader";
+import { RGBELoader } from "RGBELoader";
 
 let envMap, logo;
 
