33e9a337bcbca2861f2bc4609d8588df58609acb
goHere <clemay@gohere.tech>
2023-02-01 23:06:43 -0600

    rotate

--- src/logo.js
+++ src/logo.js
@@ -47,7 +47,7 @@ let sections = [
     "slide2",
     {
       position: new THREE.Vector3(0, 50, 50),
-      rotation: new THREE.Vector3(Math.PI / 2, 20, Math.PI),
+      rotation: new THREE.Vector3(Math.PI / 2, 10, 0),
     },
     {
       position: new THREE.Vector3(0, 0, 0),
